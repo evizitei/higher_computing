d20d2381cb252c109d713d61c1bdfd56a5c9e709
Ethan Vizitei <ethan.vizitei@gmail.com>
2011-02-02 21:23:23 -0600

    trying some placeholder formatting
    Signed-off-by: Ethan Vizitei <ethan.vizitei@gmail.com>

---
+++ placeholders.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+  printf("printing an integer: %i",45);
+  printf("...and an unsigned integer: %u",-32);
+  printf("...and a character: %c", 49);
+  printf("...and a string: %s", "Hello World");
+}
